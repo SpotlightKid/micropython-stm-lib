d067177ed6a8c8b2ff7f7443a872dce2cd54cd4e
Christopher Arndt <chris@chrisarndt.de>
2016-09-04 00:56:23 +0200

    Add encoder acceleration; rename some params and attributes
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- encoder/esp_encoder.py
+++ encoder/esp_encoder.py
@@ -6,12 +6,12 @@ Usage:
     from time import sleep_ms
     from esp_encoder import Encoder
 
-    e = Encoder(4, 5)
+    enc = Encoder(4, 5)
 
     def readloop(e):
         oldval = -1
         while True:
-            val = int(e.position)
+            val = int(enc.value)
             if oldval != val:
                 print(val)
                 oldval = val
@@ -22,48 +22,99 @@ Usage:
 
 from machine import Pin
 
-ENC_STATES = (0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0)
+
+ENC_STATES = (
+    0,   # 00 00
+    -1,  # 00 01
+    1,   # 00 10
+    0,   # 00 11
+    1,   # 01 00
+    0,   # 01 01
+    0,   # 01 10
+    -1,  # 01 11
+    -1,  # 10 00
+    0,   # 10 01
+    0,   # 10 10
+    1,   # 10 11
+    0,   # 11 00
+    1,   # 11 01
+    -1,  # 11 10
+    0    # 11 11
+)
+IRQ_MODE = Pin.IRQ_RISING | Pin.IRQ_FALLING
+ACCEL_THRESHOLD = const(5)
 
 
 class Encoder(object):
-    def __init__(self, pin_x, pin_y, pin_mode=None, scale=1,
-                 min=0, max=100, reverse=False):
-        self.pin_x = (pin_x if isinstance(pin_x, Pin) else
-                      Pin(pin_x, Pin.IN, pin_mode))
-        self.pin_y = (pin_y if isinstance(pin_y, Pin) else
-                      Pin(pin_y, Pin.IN, pin_mode))
+    def __init__(self, pin_clk, pin_dt, pin_mode=None, clicks=1,
+                 min_val=0, max_val=100, accel=0, reverse=False):
+        self.pin_clk = (pin_clk if isinstance(pin_clk, Pin) else
+                        Pin(pin_clk, Pin.IN, pin_mode))
+        self.pin_dt = (pin_dt if isinstance(pin_dt, Pin) else
+                       Pin(pin_dt, Pin.IN, pin_mode))
 
-        self.scale = scale
-        self.min = min
-        self.max = max
+        self.min_val = min_val * clicks
+        self.max_val = max_val * clicks
+        self.accel = int((max_val - min_val) / 100 * accel)
+        self.max_accel = int((max_val - min_val) / 2)
+        self.clicks = clicks
         self.reverse = 1 if reverse else -1
 
         # The following variables are assigned to in the interrupt callback,
         # so we have to allocate them here.
-        self._pos = -1
+        self._value = 0
         self._readings = 0
         self._state = 0
+        self.cur_accel = 0
 
         self.set_callbacks(self._callback)
 
     def _callback(self, line):
-        self._readings = (self._readings << 2 | self.pin_x.value() << 1 |
-                          self.pin_y.value()) & 0x0f
+        self._readings = (self._readings << 2 | self.pin_clk.value() << 1 |
+                          self.pin_dt.value()) & 0x0f
 
         self._state = ENC_STATES[self._readings] * self.reverse
 
         if self._state:
-            self._pos = min(max(self.min, self._pos + self._state), self.max)
+            self.cur_accel = min(self.max_accel, self.cur_accel + self.accel)
+
+            self._value = min(self.max_val, max(self.min_val, self._value +
+                              (1 + (self.cur_accel >> ACCEL_THRESHOLD)) *
+                              self._state))
 
     def set_callbacks(self, callback=None):
-        self.irq_x = self.pin_x.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING,
-                                    handler=callback)
-        self.irq_y = self.pin_y.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING,
-                                    handler=callback)
+        self.irq_clk = self.pin_clk.irq(trigger=IRQ_MODE, handler=callback)
+        self.irq_dt = self.pin_dt.irq(trigger=IRQ_MODE, handler=callback)
 
     @property
-    def position(self):
-        return self._pos * self.scale
+    def value(self):
+        return self._value // self.clicks
 
     def reset(self):
-        self._pos = 0
+        self._value = 0
+
+
+def test(enc=None, **kwargs):
+    from time import sleep_ms
+    rate = kwargs.pop('rate', 20)
+
+    if not isinstance(enc, Encoder):
+        kwargs.setdefault('pin_clk', 12)
+        kwargs.setdefault('pin_dt', 14)
+        kwargs.setdefault('clicks', 4)
+        enc = Encoder(**kwargs)
+
+    oldval = 0
+    while True:
+        val = enc.value
+        if oldval != val:
+            print(val)
+            oldval = val
+
+        enc.cur_accel = max(0, enc.cur_accel - enc.accel)
+
+        sleep_ms(1000 // rate)
+
+
+if __name__ == '__main__':
+    test()
