e1eb8da54b0b3eedc452f5e26fda031db49c5c03
Christopher Arndt <chris@chrisarndt.de>
2017-03-05 18:52:53 +0100

    MidiIn can filter certain event types and act as a soft MIDI thru
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- midi/midi/midiin.py
+++ midi/midi/midiin.py
@@ -7,18 +7,26 @@ from .constants import *
 class MidiIn:
     """MIDI input class."""
 
-    def __init__(self, device, callback=None, debug=False):
+    def __init__(self, device, callback=None, debug=False, softthru=False):
         if not hasattr(device, 'any'):
             raise TypeError("device instance must have a 'any' method.")
 
         if not hasattr(device, 'read'):
             raise TypeError("device instance must have a 'read' method.")
 
+        if softthru and not hasattr(device, 'write'):
+            raise TypeError("device instance must have a 'write' method if "
+                            "soft thru is enabled.")
+
         self.device = device
         self.callback = callback
         self.debug = debug
+        self.softthru = softthru
         self._msgbuf = None
         self._status = None
+        self._ignore_active_sense = False
+        self._ignore_clock = False
+        self._ignore_sysex = False
 
     def __repr__(self):
         return '<MidiIn: device={} callback={}>'.format(
@@ -36,6 +44,20 @@ class MidiIn:
             for msg in msgs:
                 self.callback(msg)
 
+    def ignore_types(self, active_sensing=False, clock=False, sysex=False):
+        """Activate filter for certain event types.
+
+        To improve performance, Active Sensing (0xFE), Timing Clock (0xF8) or
+        System Exclusive (0xF0 ... 0xF7) Messages can be filtered out, so they
+        are dropped and do not trigger the input callback function.
+
+        Pass ``active_sensing=True``, ``clock=True`` and/or ``sysex=True`` to
+        enable the filter for the respective event types.
+        """
+        self._ignore_active_sense = active_sensing
+        self._ignore_clock = clock
+        self._ignore_sysex = sysex
+
     def _error(self, msg, *args):
         if self.debug:
             import sys
@@ -52,22 +74,33 @@ class MidiIn:
         while self.device.any():
             data = self.device.read(1)[0]
 
+            if self.softthru:
+                # pass byte from MIDI in through to MIDI out
+                self.device.write(bytes([data]))
+
             if data & 0x80:
                 # A status byte
                 if TIMING_CLOCK <= data <= SYSTEM_RESET:
                     # System real-time message
-                    if data != 0xFD:
+                    if data == ACTIVE_SENSING and self._ignore_active_sensing:
+                        continue
+                    elif data == TIMING_CLOCK and self._ignore_clock:
+                        continue
+                    elif data != 0xFD:
                         msgs.append(bytearray([data]))
                     else:
                         self._error("Read undefined system real-time status "
                                     "byte 0x%0X.", data)
                 elif data == SYSTEM_EXCLUSIVE:
                     # Start of sysex message
-                    self._status = None
-                    self._msgbuf = bytearray([data])
+                    self._status = SYSTEM_EXCLUSIVE
+                    if self._ignore_sysex:
+                        self._msgbuf = None
+                    else:
+                        self._msgbuf = bytearray([data])
                 elif data == END_OF_EXCLUSIVE:
                     # End of sysex message
-                    if self._msgbuf:
+                    if self._msgbuf and not self._ignore_sysex:
                         self._msgbuf.append(data)
                         msgs.append(self._msgbuf)
 
@@ -90,6 +123,9 @@ class MidiIn:
                     self._status = data
                     self._msgbuf = bytearray([data])
             else:
+                if self._status == SYSTEM_EXCLUSIVE and self._ignore_sysex:
+                    continue
+
                 # A data byte
                 if self._status and not self._msgbuf:
                     # Running status assumed
@@ -101,9 +137,9 @@ class MidiIn:
 
                 self._msgbuf.append(data)
 
-                if (self._msgbuf[0] != SYSTEM_EXCLUSIVE and
+                if (self._status != SYSTEM_EXCLUSIVE and
                         (len(self._msgbuf) == 3 or self._msgbuf[0] & 0xF0 in
-                        (PROGRAM_CHANGE, CHANNEL_PRESSURE, MTC, SPP))):
+                         (PROGRAM_CHANGE, CHANNEL_PRESSURE, MTC, SPP))):
                     msgs.append(self._msgbuf)
                     self._msgbuf = None
 
--- midi/midi/midiin_min.py
+++ midi/midi/midiin_min.py
@@ -1,13 +1,21 @@
 from.constants_min import*
 class MidiIn:
- def __init__(self,device,callback=None,debug=False):
-  if not hasattr(device,'any'):raise TypeError("device instance must have a 'any' method.")
-  if not hasattr(device,'read'):raise TypeError("device instance must have a 'read' method.")
+ def __init__(self,device,callback=None,debug=False,softthru=False):
+  if not hasattr(device,'any'):
+   raise TypeError("device instance must have a 'any' method.")
+  if not hasattr(device,'read'):
+   raise TypeError("device instance must have a 'read' method.")
+  if softthru and not hasattr(device,'write'):
+   raise TypeError("device instance must have a 'write' method if " "soft thru is enabled.")
   self.device=device
   self.callback=callback
   self.debug=debug
+  self.softthru=softthru
   self._msgbuf=None
   self._status=None
+  self._ignore_active_sense=False
+  self._ignore_clock=False
+  self._ignore_sysex=False
  def __repr__(self):
   return '<MidiIn: device={} callback={}>'.format(self.device,'yes' if callable(self.callback)else 'no')
  def poll(self):
@@ -15,6 +23,10 @@ class MidiIn:
   if msgs and self.callback:
    for msg in msgs:
     self.callback(msg)
+ def ignore_types(self,active_sensing=False,clock=False,sysex=False):
+  self._ignore_active_sense=active_sensing
+  self._ignore_clock=clock
+  self._ignore_sysex=sysex
  def _error(self,msg,*args):
   if self.debug:
    import sys
@@ -23,15 +35,26 @@ class MidiIn:
   msgs=[]
   while self.device.any():
    data=self.device.read(1)[0]
+   if self.softthru:
+    self.device.write(bytes([data]))
    if data&0x80:
     if TIMING_CLOCK<=data<=SYSTEM_RESET:
-     if data!=0xFD:msgs.append(bytearray([data]))
-     else:self._error("Read undefined system real-time status " "byte 0x%0X.",data)
+     if data==ACTIVE_SENSING and self._ignore_active_sensing:
+      continue
+     elif data==TIMING_CLOCK and self._ignore_clock:
+      continue
+     elif data!=0xFD:
+      msgs.append(bytearray([data]))
+     else:
+      self._error("Read undefined system real-time status " "byte 0x%0X.",data)
     elif data==SYSTEM_EXCLUSIVE:
-     self._status=None
-     self._msgbuf=bytearray([data])
+     self._status=SYSTEM_EXCLUSIVE
+     if self._ignore_sysex:
+      self._msgbuf=None
+     else:
+      self._msgbuf=bytearray([data])
     elif data==END_OF_EXCLUSIVE:
-     if self._msgbuf:
+     if self._msgbuf and not self._ignore_sysex:
       self._msgbuf.append(data)
       msgs.append(self._msgbuf)
      self._msgbuf=None
@@ -39,19 +62,25 @@ class MidiIn:
     elif MIDI_TIME_CODE<=data<=TUNING_REQUEST:
      self._status=None
      self._msgbuf=None
-     if data==TUNING_REQUEST:msgs.append(bytearray([data]))
-     elif data<=SONG_SELECT:self._msgbuf=bytearray([data])
-     else:self._error("Read undefined system common status byte " "0x%0X.",data)
+     if data==TUNING_REQUEST:
+      msgs.append(bytearray([data]))
+     elif data<=SONG_SELECT:
+      self._msgbuf=bytearray([data])
+     else:
+      self._error("Read undefined system common status byte " "0x%0X.",data)
     else:
      self._status=data
      self._msgbuf=bytearray([data])
    else:
-    if self._status and not self._msgbuf:self._msgbuf=bytearray([self._status])
+    if self._status==SYSTEM_EXCLUSIVE and self._ignore_sysex:
+     continue
+    if self._status and not self._msgbuf:
+     self._msgbuf=bytearray([self._status])
     if not self._msgbuf:
      self._error("Read unexpected data byte 0x%0X."%data)
      continue
     self._msgbuf.append(data)
-    if(self._msgbuf[0]!=SYSTEM_EXCLUSIVE and(len(self._msgbuf)==3 or self._msgbuf[0]&0xF0 in(PROGRAM_CHANGE,CHANNEL_PRESSURE,MTC,SPP))):
+    if(self._status!=SYSTEM_EXCLUSIVE and(len(self._msgbuf)==3 or self._msgbuf[0]&0xF0 in(PROGRAM_CHANGE,CHANNEL_PRESSURE,MTC,SPP))):
      msgs.append(self._msgbuf)
      self._msgbuf=None
   return msgs
