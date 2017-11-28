49a86432a7e8462bfdd94eff65eb5a6b0b4d3dd6
Christopher Arndt <chris@chrisarndt.de>
2017-11-28 19:43:53 +0100

    More socket code compatibility fixes
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- picoredis/picoredis/picoredis.py
+++ picoredis/picoredis/picoredis.py
@@ -31,15 +31,13 @@ class ParseError(Exception):
 
 def encode_request(*args):
     """Pack a series of arguments into a RESP array of bulk strings."""
-    result = ["*"]
-    result.append(str(len(args)))
-    result.append(CRLF)
+    result = ["*" + str(len(args)) + CRLF]
 
     for arg in args:
         if arg is None:
             result.append('$-1' + CRLF)
         else:
-            s = str(arg) 
+            s = str(arg)
             result.append('$' + str(len(s)) + CRLF + s + CRLF)
 
     return "".join(result)
@@ -47,30 +45,55 @@ def encode_request(*args):
 
 class Redis:
     """A very minimal Redis client."""
-    
+
     def __init__(self, host='127.0.0.1', port=6379, timeout=3000, debug=False):
-        self._timeout = timeout
         self.debug = debug
-        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._sock.connect(socket.getaddrinfo(host, port)[0][-1])
-        self._sock_fd = self._sock.makefile('rb').fileno()
-        self._poller = select.poll()
-        self._poller.register(self._sock_fd, select.POLLIN)
+        self._sock = None
+        self._timeout = timeout
+        self.connect(host, port)
+
+    def connect(self, host=None, port=None):
+        if port is not None:
+            self._host = host
+
+        if port is not None:
+            self._port = port
+
+        if not self._sock:
+            self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._sock.connect(socket.getaddrinfo(self._host, self._port)[0][-1])
+            self._sock_fd = self._sock.makefile('rb')
+            try:
+                self._sock_fd = self._sock_fd.fileno()
+            except AttributeError:
+                pass
+            self._poller = select.poll()
+            self._poller.register(self._sock_fd, select.POLLIN)
+
+    def close(self):
+        if self._sock:
+            self._poller.unregister(self._sock_fd)
+            self._sock.close()
+            self._sock = None
 
     def do_cmd(self, cmd, *args):
-        req = encode_request(cmd, *args)
+        if not self._sock:
+            raise RedisError("Not connected: use 'connect()' to connect to Redis server.")
+
+        request = encode_request(cmd, *args)
 
         if self.debug:
-            print("SEND: {!r}".format(req))
+            print("SEND: {!r}".format(request))
 
-        self._sock.send(req.encode('utf-8'))
+        self._sock.send(request.encode('utf-8'))
         return self._read_response()
 
     __call__ = do_cmd
 
     def __getattr__(self, name):
-        if name.isalpha() and not name.startswith('_'):
+        if name.isalpha():
             return lambda *args: self.do_cmd(name, *args)
+
         raise AttributeError
 
     def _read_response(self):
@@ -104,17 +127,18 @@ class Redis:
         buf = b''
         pos = 0
         while not predicate(buf, pos):
-            ready = self._poller.poll(self._timeout)
-            if not ready:
+            readylist = self._poller.poll(self._timeout)
+            if not readylist:
                 raise RedisTimeout("Error reading response from Redis server within timeout.")
 
-            for fd, ev in ready:
-                if (fd == self._sock_fd and ev & select.POLLIN and not
-                        ev & (select.POLLHUP | select.POLLERR)):
+            for entry in readylist:
+                if (entry[0] is self._sock_fd and entry[1] & select.POLLIN and not
+                        entry[1] & (select.POLLHUP | select.POLLERR)):
                     buf += self._sock.recv(1)
                     pos += 1
                     break
             else:
+                self.close()
                 raise OSError("Error reading from socket.")
 
         if self.debug:
